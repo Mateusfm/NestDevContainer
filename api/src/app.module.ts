d2dde88a5c8be4f0e7f36cf50536297537ec541c
Mateus <mateusfm95@gmail.com>
2025-08-15 09:15:44 -0300

    ci: Publicando app runner

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,21 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'mysql',
-      port: 3306,
-      username: 'admin',
-      password: 'root',
-      database: 'rocketseat-db',
-      entities: [],
-      synchronize: true,
-    })
-  ],
+  imports: [ ],
   controllers: [AppController],
   providers: [AppService],
 })
